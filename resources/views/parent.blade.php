b46f6c7a0f432c8f5dcf7ec02dab3d4dfdd66c98
Iqbalaulia <16410100012@stikom.edu>
2019-06-15 20:31:45 +0700

    initial commit

---
+++ resources/views/parent.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="container">    
+        <br />
+        <h3 align="center">Laravel 5.8 Crud Tutorial</h3>
+        <br />
+        @yield('main')
+       </div>
+</body>
+
+</html>
